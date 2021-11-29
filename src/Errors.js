df733e3825724b9f1d8646396071e3874217d5ab
JOSEPH <93124106+josephdevtm@users.noreply.github.com>
2021-11-29 20:22:35 +0200

    Create Errors.js

---
+++ src/Errors.js
@@ -0,0 +1,9 @@
+class FetchFormError extends Error {
+  constructor(message, status) {
+    super(message);
+    this.status = status;
+    this.name = 'FetchFormError';
+  }
+}
+
+export { FetchFormError };
